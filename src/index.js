650fdaef57fa986df78bca7d872964a046797adf
Vasiliy Shubko <flit77@gmail.com>
2017-12-18 00:03:03 +0100

    hello world

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from 'react';
+import { Provider, connect } from 'react-redux';
+import { createStore, compose } from 'redux';
 
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'CREATE_TODO':
+      return state.concat([action.text]);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  todos,
+  undefined,
+  compose(window.devToolsExtension ? window.devToolsExtension() : f => f)
+);
+
+class App extends Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
